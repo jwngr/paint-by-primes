2e92078c3480acf65876d7e604c88f5238d69e0e
jwngr <wenger.jacob@gmail.com>
2019-01-11 14:45:03 +0000

    Add access log and capture output to gunicorn config file

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -4,4 +4,6 @@ import multiprocessing
 name = 'primes'
 bind = '127.0.0.1:8000'
 errorlog = 'errors.log'
+accesslog = 'access.log'
+capture_output = True
 workers = multiprocessing.cpu_count() * 2 + 1
