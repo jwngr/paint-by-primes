72838c2add3bebfdc2da0478555c6d121b0853c5
jwngr <wenger.jacob@gmail.com>
2019-01-17 11:23:25 +0000

    Enable website Firebase deployment

--- website/src/loadFirebase.js
+++ website/src/loadFirebase.js
@@ -5,7 +5,18 @@ import firebaseConfig from './loadConfig';
 require('firebase/storage');
 require('firebase/firestore');
 
-firebase.initializeApp(firebaseConfig);
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (error) {
+  // eslint-disable-next-line
+  console.log('[ERROR] Failed to initialize Firebase:', error);
+  // eslint-disable-next-line
+  console.log(
+    '[DEBUG] Attempted to initialize Firebase with the following config:',
+    firebaseConfig
+  );
+  throw error;
+}
 
 export default firebase;
 
